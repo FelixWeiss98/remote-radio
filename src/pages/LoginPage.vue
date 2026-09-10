b661a5f8459b01b31f3bdc2c15a63d042e1a5c60
FelixWeiss98 <weissfelix@proton.me>
2026-09-10 20:33:46 +0200

    chore(ui): adds basic colors

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,9 +1,32 @@
 <template>
   <div class="login-page">
-    <h1>Login</h1>
+    <div class="login-page-form">
+      <h1>Login</h1>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+@use '../assets/css/abstracts' as *;
+
+.login-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: $color-grayish-white;
+}
+
+.login-page-form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: $color-offwhite;
+  width: 30rem;
+  height: 40rem;
+  border-radius: 2rem;
+}
+</style>
